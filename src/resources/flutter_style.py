2cc52c34c9d3bd74599ecc34ab97d97c1d7ebf4d
lee kang hun <chinggu88@cuunit.com>
2025-07-28 14:45:33 +0900

    다시 reset...

--- src/resources/flutter_style.py
+++
@@ -1,9 +0,0 @@
-from fastmcp import FastMCP
-
-
-def register_resources(mcp: FastMCP):
-    """MCP 리소스 등록"""
-    
-    @mcp.resource('resource://flutter_res')
-    def flutter_resource():
-        return open("flutter_style.md", encoding="utf-8").read()
